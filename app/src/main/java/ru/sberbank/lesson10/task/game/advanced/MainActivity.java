745d7244e60b13a0c286e89223600c5658f7fd01
w3dip <w3dipmail@gmail.com>
2019-02-02 13:28:23 +0300

    made looper

--- app/src/main/java/ru/sberbank/lesson10/task/game/advanced/MainActivity.java
+++ app/src/main/java/ru/sberbank/lesson10/task/game/advanced/MainActivity.java
@@ -3,6 +3,8 @@ package ru.sberbank.lesson10.task.game.advanced;
 import android.app.Activity;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -30,10 +32,15 @@ public class MainActivity extends Activity {
     private HashMap<Integer, Bucket> buckets;
     private Map<Integer, ImageView> bucketViews;
 
+    private Handler handler;
+    private Runnable runnable;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        handler = new Handler(Looper.getMainLooper());
 
         if (savedInstanceState != null) {
             isStarted = savedInstanceState.getBoolean(GAME_IS_STARTED);
@@ -46,83 +53,56 @@ public class MainActivity extends Activity {
             buckets.put(3, of(R.id.bucket3));
         }
 
-        final Button button = findViewById(R.id.newGame);
-        button.setVisibility(isStarted ? View.INVISIBLE : View.VISIBLE);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                ballPosition = new Random().nextInt(3) + 1;
-                isStarted = true;
-                button.setVisibility(View.INVISIBLE);
-                for(Bucket bucket : buckets.values()) {
-                    ImageView bucketImageView = getBucket(bucket.getBucketId());
-                    bucketImageView.setImageDrawable(getResources().getDrawable(R.mipmap.bucket));
-                    bucket.setVisible(true);
-                    bucketImageView.setVisibility(View.VISIBLE);
-                }
+        final Button newGameBtn = findViewById(R.id.newGame);
+        newGameBtn.setVisibility(isStarted ? View.INVISIBLE : View.VISIBLE);
+        newGameBtn.setOnClickListener(v -> {
+            handler.removeCallbacksAndMessages(null);
+            ballPosition = new Random().nextInt(3) + 1;
+            isStarted = true;
+            newGameBtn.setVisibility(View.INVISIBLE);
+            for(Bucket bucket : buckets.values()) {
+                ImageView bucketImageView = getBucket(bucket.getBucketId());
+                bucketImageView.setImageDrawable(getResources().getDrawable(R.mipmap.bucket));
+                bucket.setVisible(true);
+                bucketImageView.setVisibility(View.VISIBLE);
             }
         });
 
         bucketViews = Maps.newHashMap();
 
         for(Bucket bucket : buckets.values()) {
-            int id = bucket.getBucketId();
-            ImageView bucketImageView = getBucket(id);
+            ImageView bucketImageView = getBucket(bucket.getBucketId());
             bucketImageView.setVisibility(bucket.isVisible() ? View.VISIBLE : View.INVISIBLE);
-            bucketViews.put(id, bucketImageView);
+            bucketViews.put(Integer.parseInt(bucketImageView.getTag().toString()), bucketImageView);
         }
 
-        for (ImageView bucketView : bucketViews.values()) {
-            bucketView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(final View v) {
-                    final Resources res = getResources();
-                    String result;
-                    final int selectedBucketNum = Integer.parseInt(v.getTag().toString());
-                    if (isStarted) {
-                        if ( selectedBucketNum == ballPosition) {
-                            result = res.getString(R.string.win);
-                        } else {
-                            result = res.getString(R.string.looose);
-                        }
-                    } else {
-                        result = res.getString(R.string.isOver);
-                    }
+        for (Map.Entry<Integer, ImageView> entry : bucketViews.entrySet()) {
+            entry.getValue().setOnClickListener(v -> {
+                int index = 1;
+                final Resources res = getResources();
+                String result;
+                final int selectedBucketNum = entry.getKey();
+                if (!isStarted) {
+                    result = res.getString(R.string.isOver);
                     Toast.makeText(MainActivity.this, result, Toast.LENGTH_SHORT).show();
-
-                    Bucket correctBucket = buckets.get(ballPosition);
-                    if (correctBucket == null) {
-                        return;
-                    }
-                    final ImageView correctBucketImageView = getBucket(correctBucket.getBucketId());
-                    final int correctBucketNum = Integer.parseInt(correctBucketImageView.getTag().toString());
-
-                    v.postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            v.setVisibility(View.INVISIBLE);
-                            for (final ImageView view : bucketViews.values()) {
-                                int currentBucketNum = Integer.parseInt(view.getTag().toString());
-                                if (currentBucketNum != selectedBucketNum && currentBucketNum != correctBucketNum) {
-                                    view.postDelayed(new Runnable() {
-                                        @Override
-                                        public void run() {
-                                            view.setVisibility(View.INVISIBLE);
-                                            correctBucketImageView.postDelayed(new Runnable() {
-                                                @Override
-                                                public void run() {
-                                                    correctBucketImageView.setImageDrawable(res.getDrawable(R.mipmap.ball));
-                                                }
-                                            }, 1000);
-                                        }
-                                    }, 1000);
-                                }
-                            }
-                        }
-                    }, 1000);
-
-                    button.setVisibility(View.VISIBLE);
-                    isStarted = false;
+                    return;
+                } else if (selectedBucketNum == ballPosition) {
+                    result = res.getString(R.string.win);
+                } else {
+                    result = res.getString(R.string.looose);
                 }
+
+                startAnimation(v, selectedBucketNum, index);
+                handler.postDelayed(() -> Toast.makeText(MainActivity.this, result, Toast.LENGTH_SHORT).show(), 1000);
+                index++;
+
+                for (Map.Entry<Integer, ImageView> entry1 : Maps.filterKeys(bucketViews, input -> input != selectedBucketNum).entrySet()) {
+                    startAnimation(entry1.getValue(), entry1.getKey(), index);
+                    index++;
+                }
+
+                newGameBtn.setVisibility(View.VISIBLE);
+                isStarted = false;
             });
         }
     }
@@ -139,12 +119,21 @@ public class MainActivity extends Activity {
         return (ImageView)findViewById(id);
     }
 
-    private Runnable work(final View v) {
-        return new Runnable() {
-            @Override
-            public void run() {
-                v.setVisibility(View.INVISIBLE);
-            }
-        };
+    private Runnable openEmptyBucket(final View v) {
+        return () -> v.setVisibility(View.INVISIBLE);
+    }
+
+    private Runnable openBallBucket(final View v) {
+        return () -> ((ImageView)v).setImageDrawable(getResources().getDrawable(R.mipmap.ball));
+    }
+
+    private void startAnimation(View v, int currentBucketNum, int index) {
+        if (currentBucketNum == ballPosition) {
+            runnable = openBallBucket(v);
+            handler.postDelayed(runnable, 1000 * index);
+        } else {
+            runnable = openEmptyBucket(v);
+            handler.postDelayed(runnable, 1000 * index);
+        }
     }
 }
